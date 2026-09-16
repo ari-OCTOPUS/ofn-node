c352f6a34f01661c11ffdb0806cc2ec59400583e
ari-vault <paintingdesign76@gmail.com>
2026-09-16 23:43:28 +1000

    durability snapshot v4 [54/87] _Templates

--- _Archive/OCTOPUS-nervous-system-snapshot-2026-07-12/nervous-system/refresh-live-data.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-set PYTHONUTF8=1
-cd /d "%~dp0"
-echo Refreshing OCTOPUS live data...
-python extract_live_data.py
-python extract_ops_data.py
-python extract_graph.py
-echo Done.
